6d5ae0eff0633cc15fa3fff571c445df22db02e3
M.Silva <msilva@kiubo.pt>
2023-01-20 16:49:13 +0000

    read config file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true);
+
+var config = builder.Build();
+
+var appName = config["ConnectionString"];
+Console.WriteLine(appName);
+
+var rmqHost = config["MassTransit:Host"];
+Console.WriteLine(rmqHost);
---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Dependency:
+    dotnet add package Microsoft.Extensions.Configuration.Json
+    
+## Run:
+    dotnet run
